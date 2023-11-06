158f4858b4904ef23cc216e849b757d176778198
Adrian Gąsiorek <adriangasiorek@interia.pl>
2023-11-07 00:49:59 +0100

    changing nick feature works

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,10 +2,41 @@ import { useState } from "react"
 import { Login } from "./components/Login"
 import { BalanceShow } from "./features/balance/balanceShow"
 import { Nickname } from "./features/nickname/nickname"
+import { useSelector, useDispatch } from 'react-redux'
+import { setNickname } from './features/nickname/nicknameSlice';
+
 
 export const Navbar = () =>{
 
+    const dispatch = useDispatch()
+
+
     const [isModalActive, setIsModalActive] = useState(false)
+
+    const handleToggleModal = () =>{
+        setIsModalActive(!isModalActive)
+    }
+
+
+    const handleStartGame = (e) =>{
+
+        const newNick = e.target[0].value
+
+        e.preventDefault()
+
+        let localStorageInfo = JSON.parse(localStorage.getItem('roulette-user-info')) 
+        localStorageInfo.nickName = newNick
+
+        if(newNick.length > 0){
+            localStorage.setItem('roulette-user-info', JSON.stringify(localStorageInfo));
+            dispatch(setNickname(newNick))
+        }
+
+        setIsModalActive(false)
+
+
+
+    }
 
 
     console.log('kurwa xd', isModalActive)
@@ -14,13 +45,12 @@ export const Navbar = () =>{
             <p className="nav-title">Xoviec-Roll</p>
             <div className="user-dock">
                 <BalanceShow/>
-                <button onClick={(()=>setIsModalActive(!isModalActive))}>
+                <button onClick={handleToggleModal}>
                     <Nickname />
                 </button>
             </div>
             {
-                isModalActive && <Login/>
- 
+                isModalActive && <Login handleStartGame={handleStartGame}/>
             }
             
         </nav>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,24 @@
 
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 export const Login = ({handleStartGame}) =>{
+
+    const [nick, setUserNick] = useState(useSelector((state) => state.nickname.value))
+
+    const handleEditNick = (e) =>{
+        setUserNick(e.target.value)
+    }
+
+
+    console.log(nick)
 
     return(
         <div className="login-dialog">
             <container>
                 <h1 className="login-title">Input your nickname</h1>
                 <form className="login-form" action="" onSubmit={handleStartGame}>
-                    <input type="text" placeholder="Nickname" />
+                    <input type="text" placeholder="Nickname" onChange={handleEditNick} value={nick} />
                     <button type="submit">Set nickname</button>
                 </form>
             </container>
